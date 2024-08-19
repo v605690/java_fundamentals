1439d8fc8644e364a2902c6eec58e843d31b141d
v605690 <v605690@gmail.com>
2024-08-19 14:15:37 -0500

    Complete Labs Array Excercise_05

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -9,5 +9,16 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
 
+        int count = 0;
+
+        int[] nums = {33, 5, 12, 23, 66, 83, 18, 19};
+
+        for (int i = nums.length - 1; i >=0; i-- ) {
+            if (count++ % 2 != 0) {
+                System.out.println(nums[i]);
+            }
+        }
+    }
 }
