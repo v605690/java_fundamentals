7e4fe3df9a78eb90513c0749260d11e65f23c1f9
v605690 <v605690@gmail.com>
2024-08-19 11:37:10 -0500

    Complete Labs Array Excercise_02

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -13,9 +13,13 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
+        int index = -1;
         // write code here
-
-
+        for (int i = 0; (i < array.length) && (index == -1); i++) {
+            if (array[i] == 9) {
+                index = i;
+            }
+        }
+        System.out.println(index);
     }
 }
