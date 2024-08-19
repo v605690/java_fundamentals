4ce7e9e2e24321c88c212e75977c11c3b7d10550
v605690 <v605690@gmail.com>
2024-08-19 10:48:02 -0500

    Complete Labs Array Excercise_01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -13,5 +13,18 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 5, 8, 4, 13, 7, 9, 23, 19, 33};
 
+        int sum = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+               sum = sum + nums[i];
+        }
+        System.out.println("The Sum is: " + sum);
+
+        double average = sum / nums.length;
+
+        System.out.println("The Average is: " + average);
+    }
 }
