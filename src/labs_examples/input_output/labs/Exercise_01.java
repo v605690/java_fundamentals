9fa10f21b48990d5f1d110fe5cf2d396856e1fe6
v605690 <v605690@gmail.com>
2024-12-30 21:19:26 -0600

    Complete Java 301 Chapter 3 Lab Exercise 01

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -11,8 +13,25 @@ package labs_examples.input_output.labs;
 
 class Example {
     public static void main(String[] args) {
+        FileInputStream inputStream = null;
+        FileOutputStream outputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
 
+        String dataIn = "src/labs_examples/input_output/files/DataInput.txt";
+        String dataOut = "src/labs_examples/input_output/files/DataOutput.txt";
 
+        try (BufferedInputStream bufInStr = new BufferedInputStream(new FileInputStream(dataIn));
+            BufferedOutputStream bufOuStr = new BufferedOutputStream(new FileOutputStream(dataOut))) {
 
+            byte[] buffer = new byte[5];
+            int bytesRead;
+
+            while ((bytesRead = bufInStr.read(buffer)) != -1) {
+                bufOuStr.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException exc) {
+                exc.printStackTrace();
+        }
     }
 }
