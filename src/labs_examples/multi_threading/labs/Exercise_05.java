e181fae6997564e37dee1b4ba06b7efc2c6885bb
v605690 <v605690@gmail.com>
2025-01-06 19:34:32 -0600

    Complete Java 301 Chapter 4 Labs 5 and 6

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -5,3 +5,20 @@ package labs_examples.multi_threading.labs;
  *
  *      Demonstrate the use of a wait() and notify()
  */
+
+/*
+Not too sure on wait() and notify()
+ */
+class WaitNotifyDemo {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Stage 1 complete");
+        synchronized (args) {
+            System.out.println("Stage 2 complete");
+            args.notify();
+            System.out.println("Please wait....");
+            args.wait(1000);
+            System.out.println("Stage 3 complete");
+            System.out.println("All stages are now complete!!");
+        }
+    }
+}
--- src/labs_examples/multi_threading/labs/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06.java
@@ -6,3 +6,41 @@ package labs_examples.multi_threading.labs;
  *      Write a program that will print 1-100 sequentially from at least two distinct threads. For instance, thread1 will
  *      print "1", then thread2 will print "2", then thread1 will print "3", then thread2 will print "4" and so on.
  */
+
+class NumberGroup {
+    private static int minNum = 1;
+    private static int maxNum = 100;
+    public static void main(String[] args) {
+
+        Thread evenThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (minNum <= maxNum) {
+                    synchronized (NumberGroup.class) {
+                        if (minNum % 2 == 0) {
+                            System.out.println(Thread.currentThread().getName() + ": " + minNum);
+                            minNum++;
+                        }
+                    }
+                }
+            }
+        }, "Even Thread");
+
+        Thread oddThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (minNum <= maxNum) {
+                    synchronized (NumberGroup.class) {
+                        if (minNum % 2 != 0) {
+                            System.out.println(Thread.currentThread().getName() + ": " + minNum);
+                            minNum++;
+                        }
+                    }
+                }
+            }
+        }, "Odd Thread");
+
+        evenThread.start();
+        oddThread.start();
+    }
+}
