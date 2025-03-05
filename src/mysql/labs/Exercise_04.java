3712d9d8d7c4c708477db925051f432361b89979
v605690 <v605690@gmail.com>
2025-03-04 20:52:44 -0600

    Complete Java 301 Chapter 8 Labs 8, labs Excercise 02, Exercise 03, Exercise 04

--- src/mysql/labs/Exercise_04.java
+++ src/mysql/labs/Exercise_04.java
@@ -21,27 +21,102 @@ package mysql.labs;
 *
  */
 
+import java.sql.*;
+
 public class Exercise_04 {
 
-    public static void main(String[] args) {
+    public static <preparedStatement> void main(String[] args) {
         /*
         Just as a casual example - each of these operations should be in it's own method. Feel free to
         create all required classes/methods to accomplish this.
 
-        createFlight(...);
-        queryFlight(...);
-        updateFlight(...);
-        deleteFlight(...);
+        createFlight(...); -> INSERT -> executeUpdate()
+        queryFlight(...); -> SELECT -> executeQuery()
+        updateFlight(...); -> UPDATE -> executeUpdate()
+        deleteFlight(...); -> DELETE -> executeUpdate()
+
 
         createPassenger(...);
         queryPassenger(...);
         updatePassenger(...);
         deletePassenger(...);
 
-        ...
-
-
          */
-    }
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String connectionString = "jdbc:mysql://localhost/airTravelDB?"
+                    +"user=root&password=K3yst0n3!!&useSSL=false";
+            Connection connection = DriverManager.getConnection(connectionString);
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM airTravelDB.flights");
+
+            while (resultSet.next()) {
+                System.out.print("\n" + "flight ID " + resultSet.getInt("flight_id"));
+                System.out.print(" " + "Flight Status: "+ resultSet.getString("flight_status"));
+            }
+                System.out.println();
+
+             int i = statement.executeUpdate(
+                        "INSERT INTO tickets (ticket_id, passenger_id, seat_number, ticket_price, ticket_type) " +
+                                "VALUES (37, 108, 'F1C', 950, 'business')");
+            System.out.println(i);
+
+            int u = statement.executeUpdate(
+                 "UPDATE tickets SET ticket_type = 'business'" +
+                         "WHERE ticket_id = 9");
+            System.out.println(u);
+
+            int d = statement.executeUpdate(
+                  "DELETE FROM tickets WHERE ticket_id > 14 AND ticket_id < 36");
+            System.out.println(d);
+
+            System.out.println();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                  "SELECT * FROM airTravelDB.airlines WHERE airline_name = ?"
+            );
+            preparedStatement.setString( 1, "united_airlines");
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.print("flight ID " + resultSet.getInt("airline_id"));
+                System.out.println(" -> " + " Airliner -> " + resultSet.getString("airline_name"));
+            }
+
+            PreparedStatement preparedStatement2 = connection.prepareStatement(
+                   "INSERT INTO airTravelDB.airlines (airline_name)" +
+                           "VALUES (?)");
+            preparedStatement2.setString(1, "silver_airways");
+
+            int pi = preparedStatement2.executeUpdate();
+
+            PreparedStatement prepareStatement = connection.prepareStatement(
+                            "UPDATE airTravelDB.airlines SET airline_name = ? WHERE airline_id = 1008");
+
+                   prepareStatement.setString(1, "allegiant_air");
+
+                    int pu = prepareStatement.executeUpdate();
+
+           PreparedStatement prepStatement2 = connection.prepareStatement(
+                  "DELETE FROM airTravelDB.airlines WHERE airline_name = ?");
+
+                   prepStatement2.setString(1, "frontier_airlines");
+
+                  int pd = prepStatement2.executeUpdate();
+
+
+        } catch (SQLException e) {
+            System.out.println("Exception occurred");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Exception occurred +"
+                    + " - driver not found on classpath");
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
 }
