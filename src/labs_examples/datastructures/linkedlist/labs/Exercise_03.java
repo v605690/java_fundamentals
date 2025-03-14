860b3d60b8e683c7d3f0dac02d7db8dd704cb7a6
v605690 <v605690@gmail.com>
2025-03-14 08:19:00 -0500

    Complete Java 301 Chapter 9 Data Structures, Exercise 03

--- src/labs_examples/datastructures/linkedlist/labs/Exercise_03.java
+++ src/labs_examples/datastructures/linkedlist/labs/Exercise_03.java
@@ -5,3 +5,66 @@ package labs_examples.datastructures.linkedlist.labs;
  *
  *      Please create your own custom doubly-LinkedList from start to finish :) You got this!
  */
+class DoublyLinkedList<P> {
+    private Node head;
+    private Node tail;
+
+    class Node<P> {
+        P item;
+        Node prev;
+        Node next;
+
+        public Node(P item, Node prev, Node next) {
+            this.item = item;
+            this.prev = null;
+            this.next = null;
+        }
+
+        public Node(P item) {
+            this.item = item;
+            this.next = null;
+        }
+    }
+
+        public void forward() {
+            Node newNode = head;
+            while (newNode != null) {
+                System.out.println(newNode.item + " ");
+                newNode = newNode.next;
+            }
+        }
+
+        public void reverse() {
+            Node newNode = tail;
+            while (newNode != null) {
+                System.out.println(newNode.item + " ");
+                newNode = newNode.prev;
+            }
+        }
+        public void addFirst(P item) {
+            Node newNode = new Node(item);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+        }
+    }
+}
+
+class doublyLinkedListController {
+    public static void main(String[] args) {
+        DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList();
+        doublyLinkedList.addFirst("hammer");
+        doublyLinkedList.addFirst("nail");
+        doublyLinkedList.addFirst("drill");
+        doublyLinkedList.addFirst("saw");
+        doublyLinkedList.forward();
+        doublyLinkedList.reverse();
+        doublyLinkedList.addFirst("screws");
+        doublyLinkedList.addFirst("2x4");
+        doublyLinkedList.reverse();
+    }
+}
