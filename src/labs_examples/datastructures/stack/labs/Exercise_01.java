6e93a8d215cc5123b0622316bfde0169b8cf8604
v605690 <v605690@gmail.com>
2025-03-14 15:18:20 -0500

    Complete Java 301 Chapter 9 Data Structures - Stacks, Exercise 01

--- src/labs_examples/datastructures/stack/labs/Exercise_01.java
+++ src/labs_examples/datastructures/stack/labs/Exercise_01.java
@@ -1,8 +1,59 @@
 package labs_examples.datastructures.stack.labs;
 
+import java.util.LinkedList;
+
 /**
  *      Demonstrate your mastery of Java's built-in Stack class by completing the following:
  *
  *      1) instantiate a new Stack
  *      2) demonstrate the use of EVERY method in the Stack class - there are 5
  */
+
+class CustomStackDemo<P> {
+    LinkedList<P> linkedList = new LinkedList();
+
+    public void push(P item) {
+        linkedList.addFirst(item);
+    }
+
+    public P pop() {
+        try {
+            return linkedList.removeFirst();
+        } catch (NullPointerException e) {
+            System.out.println("Exception" + e);
+            return null;
+        }
+    }
+
+    public boolean isEmpty() {
+        return linkedList.isEmpty();
+    }
+    public void clear() {
+        linkedList.clear();
+    }
+
+    public P peek() {
+        return linkedList.peek();
+    }
+}
+
+class StackController {
+    public static void main(String[] args) {
+        CustomStackDemo<String> cSD = new CustomStackDemo<>();
+
+        cSD.push("Macbook Air M3");
+        cSD.push("LG 46 CS Monitor");
+        cSD.clear();
+        cSD.push("Logic MX Master 2x");
+        cSD.push("Neo Ergo Keyboard");
+        cSD.push("CalDigit Hub");
+        cSD.push("ipad pro");
+        System.out.println(cSD.isEmpty());
+        System.out.println(cSD.peek());
+
+        while (!cSD.isEmpty()) {
+            System.out.print(cSD.pop() + " ");
+        }
+        System.out.println();
+    }
+}
