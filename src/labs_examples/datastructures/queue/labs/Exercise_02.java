45c708f4e683ff9f825588fce18791084b4d5081
v605690 <v605690@gmail.com>
2025-03-19 13:46:09 -0500

    Complete Java 301 Chapter 9 Data Structures - Queues, Exercise 02

--- src/labs_examples/datastructures/queue/labs/Exercise_02.java
+++ src/labs_examples/datastructures/queue/labs/Exercise_02.java
@@ -16,3 +16,77 @@ package labs_examples.datastructures.queue.labs;
  *      6) contain a method to print out the data of all elements in the Queue
  *
  */
+
+class GenericQueue<P> {
+    private P[] array;
+    private int first;
+    private int end;
+    private int size;
+
+    public GenericQueue(int size) {
+        this.array = (P[]) new Object[size];
+        this.first = first;
+        this.end = end;
+        this.size = size;
+    }
+
+    public int size() {
+        return array.length;
+    }
+
+    public P peakFirst() {
+        return array[first];
+    }
+
+    public P peakLast() {
+        return array[--end];
+    }
+
+    public void add(P item) {
+        array[++end] = item;
+    }
+
+    public void enQueue(P item) {
+        array[end] = item;
+        end = (end + 1) % array.length;
+        size++;
+    }
+
+    public P dequeue() {
+        P item = array[first];
+        first = (first + 1) % array.length;
+        size--;
+        return item;
+    }
+
+    public void printQueue() {
+            for (int i = first; i <= end; i++) {
+                System.out.println(i + " " + array[i]);
+            }
+            System.out.println(end);
+
+    }
+
+}
+
+
+class GenericQueueController {
+    public static void main(String[] args) {
+        GenericQueue<String> arr = new GenericQueue<>(10);
+
+        arr.add("Water");
+        arr.add("Milk");
+        arr.add("Bread");
+        arr.add("Chicken");
+        arr.add("pasta");
+        //arr.printArray();
+        System.out.println("---------------------------------");
+        arr.enQueue("Orange Juice");
+        arr.enQueue("Butter");
+        arr.printQueue();
+        System.out.println(arr.size());
+        System.out.println(arr.peakFirst());
+        System.out.println(arr.peakLast());
+        System.out.println(arr.dequeue());
+    }
+}
