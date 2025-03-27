d360c4b80a4329fbcafc8456e2e602d4e8bdafad
v605690 <v605690@gmail.com>
2025-03-27 17:43:19 -0500

    Complete Java 301 Chapter 9 Data Structures - HashMaps, Exercise 03

--- src/labs_examples/datastructures/hashmap/labs/Exercise_03.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_03.java
@@ -1,5 +1,10 @@
 package labs_examples.datastructures.hashmap.labs;
 
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  *      HashMaps Exercise_02
  *
@@ -19,3 +24,103 @@ package labs_examples.datastructures.hashmap.labs;
  *      3) search for 100 elements
  *      4) delete 100 elements
  */
+
+class megaDTS {
+    public static void main(String[] args) {
+
+        long beforeTimer = System.currentTimeMillis();
+        Timestamp ts1 = new Timestamp(beforeTimer);
+        System.out.println(ts1);
+
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        for (int i = 0; i < 100; i++) {
+            linkedList.add(" LinkList");
+            linkedList.add(" Data\n" + i);
+//            try {
+//                Thread.sleep(300);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : linkedList) {
+            sb.append(str);
+        }
+        String combinedString = sb.toString();
+        System.out.println(combinedString);
+
+        for (int i = 0; i < 100; i++) {
+            linkedList.set(i, " Updated LinkListed Data\n" + i);
+
+        }
+
+        StringBuilder sb2 = new StringBuilder();
+        for (String str : linkedList) {
+            sb2.append(str);
+        }
+        String combinedString2 = sb2.toString();
+        System.out.println(combinedString2);
+
+        long afterTimer = System.currentTimeMillis();
+        Timestamp ts2 = new Timestamp(afterTimer);
+        System.out.println(ts2);
+
+        var contains = linkedList.contains("New");
+        System.out.println(contains);
+        System.out.println("----------------------");
+        Stack<String> demoStack = new Stack<>();
+
+        if (demoStack.empty()) {
+            System.out.println("Stack is empty");
+        for (int i = 0; i < 100 + 1; i++) {
+            demoStack.push(i + " Loaded");
+            demoStack.push(" Stack");
+            demoStack.push(" Data\n");
+        }
+
+        StringBuilder sb3 = new StringBuilder();
+        for (String str : demoStack) {
+            sb3.append(str);
+        }
+        String combinedString3 = sb3.toString();
+        System.out.println(combinedString3);
+        }
+
+        var results = (demoStack.equals(" Stack Data\n"));
+        System.out.println(results);
+
+        for (String str : demoStack) {
+            System.out.println(str);
+        }
+        demoStack.clear();
+
+        System.out.println("---------------------------");
+
+        for (String str : demoStack) {
+            System.out.println(str);
+        }
+        System.out.println("Stack Empty");
+
+        System.out.println("-----------------------------");
+
+        HashMap<String, Integer> dataDog = new HashMap<>();
+
+        if (demoStack.empty()) {
+            System.out.println("Stack is empty");
+            for (int i = 0; i < 100 + 1; i++) {
+                dataDog.put("dog", 4);
+                demoStack.push(" Stack");
+                demoStack.push(" Data\n");
+            }
+
+            StringBuilder sb3 = new StringBuilder();
+            for (String str : demoStack) {
+                sb3.append(str);
+            }
+            String combinedString3 = sb3.toString();
+            System.out.println(combinedString3);
+        }
+    }
+}
